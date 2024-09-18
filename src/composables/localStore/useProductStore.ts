630c431eeb8c0e929b80159ebb8e99e0054e06df
dahi.hichem <dahi.hichem@smeetz.com>
2024-09-18 17:20:16 +0100

    add product store

--- src/composables/localStore/useProductStore.ts
+++ src/composables/localStore/useProductStore.ts
@@ -1,5 +1,6 @@
+import type { Product } from '@/models/models'
 import { useLocalStorage } from '@vueuse/core'
 
-const products = useLocalStorage('products', [] as any[])
+const products = useLocalStorage('products', [] as Product[])
 
 export default products
