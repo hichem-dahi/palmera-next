2fc0af88c57ae10c006fea9c84da945c0bcc42b0
dahi.hichem <dahi.hichem@smeetz.com>
2024-09-18 17:19:09 +0100

    add models

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,4 +1,38 @@
 export enum HistoryType {
-  dept = 1,
-  absence
+  Dept = 1,
+  Absence
+}
+
+export interface Order {
+  id: string
+  client_id: string
+  order_lines: OrderLine[]
+  total_price: number
+  paid_price: number
+  date: Date
+}
+
+export interface OrderLine {
+  id: string
+  product_id: string
+  qte: number | null
+  total_price: number
+}
+
+export interface Client {
+  id: string
+  name: string
+  phone: string
+  rc: string
+  nif: string
+  art: string
+  address: string
+  activity: string
+}
+
+export interface Product {
+  id: string
+  name: string
+  price: number | null
+  qte: number | null
 }
