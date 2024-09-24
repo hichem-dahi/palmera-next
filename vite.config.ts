5316f06f9a1321ceb93893e4d15b75715b803201
Hichem Dahi <hichemdahi57@gmail.com>
2024-09-24 13:12:16 +0100

    ad pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify()],
+  plugins: [vue(), vuetify(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
