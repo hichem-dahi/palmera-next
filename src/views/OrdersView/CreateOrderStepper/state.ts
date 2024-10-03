8fb63efaddf41bd9e14c5261670439bbaf978a4f
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-03 05:19:17 +0100

    add tva, ttc to watchEffect

--- src/views/OrdersView/CreateOrderStepper/state.ts
+++ src/views/OrdersView/CreateOrderStepper/state.ts
@@ -1,10 +1,10 @@
 import { reactive, ref, watchEffect } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
-import { max, sum } from 'lodash'
+import { max, round, sum } from 'lodash'
 
 import orders from '@/composables/localStore/useOrdersStore'
 
-import { ConsumerType, DocumentType, type OrderLine } from '@/models/models'
+import { ConsumerType, DocumentType, type Delivery, type OrderLine } from '@/models/models'
 
 const consumerType = ref<ConsumerType>()
 const documentType = ref(DocumentType.DeliveryNote)
@@ -33,22 +33,26 @@ const form = reactive({
     phone: '',
     matricule: '',
     destination: ''
-  },
+  } as Delivery | undefined,
   payment_method: '',
   paid_price: 0,
-  total_price: 0
+  total_price: 0,
+  tva: 0,
+  ttc: 0
 })
 
 watchEffect(() => {
   form.total_price = sum(form.order_lines.map((e) => e.total_price))
   form.index = (max(orders.value.map((o) => o.index)) || 0) + 1
-
+  if (documentType.value == DocumentType.Invoice) form.delivery = undefined
   if (consumerType.value == ConsumerType.Company) {
     form.individual = {
       id: '',
       name: '',
       phone: ''
     }
+    form.tva = round((form.total_price * 19) / 100, 0)
+    form.ttc = round((form.total_price * 119) / 100, 0)
   } else if (consumerType.value == ConsumerType.Individual) {
     form.company = undefined
   }
