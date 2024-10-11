8183cebf372d618b09f45a166d02decd0cf8896a
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-11 16:01:04 +0100

    add possibility to bypass to selectConsumer step

--- src/views/OrdersView/CreateOrderStepper.vue
+++ src/views/OrdersView/CreateOrderStepper.vue
@@ -1,101 +1,137 @@
-<!-- eslint-disable vue/valid-v-slot -->
 <template>
-  <v-stepper v-model="e1">
+  <v-stepper v-model="step">
     <template v-slot:default>
       <v-stepper-header>
-        <template v-for="n in steps" :key="`${n}-step`">
-          <v-stepper-item
-            :complete="e1 > n"
-            :step="`Step {{ n }}`"
-            :value="n"
-          ></v-stepper-item>
+        <template v-for="n in numericSteps" :key="`${n}-step`">
+          <div v-if="n == Steps.SelectConsumer && consumerPicked ? false : true">
+            <v-stepper-item :complete="step > n" :step="`Step {{ n }}`" :value="n">
+            </v-stepper-item>
 
-          <v-divider
-            v-if="n !== steps"
-            :key="n"
-          ></v-divider>
+            <v-divider v-if="n !== numericSteps[numericSteps.length - 1]" :key="n" />
+          </div>
         </template>
       </v-stepper-header>
 
       <v-stepper-window>
-          <form>
-            <v-stepper-window-item :value="1">
-              <SelectConsumer>
-                <template v-slot:actions="{ v }">
-                  <v-card-actions>
-                    <v-btn @click="dialog">{{ $t('cancel') }}</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn @click="nextStep(v)">{{ $t('next') }}</v-btn>
-                  </v-card-actions>
-                </template>
-              </SelectConsumer>
-            </v-stepper-window-item>
+        <form>
+          <!-- Step 1: SelectConsumer - Only displayed if no consumer in route query -->
+          <v-stepper-window-item :value="Steps.SelectConsumer">
+            <SelectConsumer>
+              <template v-slot:actions="{ v }">
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn @click="nextStep(v)">{{ $t('next') }}</v-btn>
+                </v-card-actions>
+              </template>
+            </SelectConsumer>
+          </v-stepper-window-item>
 
-            <v-stepper-window-item :value="2">
-              <CreateOrder>
-                <template v-slot:actions="{ v }">
-                  <v-card-actions>
-                    <v-btn @click="e1--">{{ $t('back') }}</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn @click="nextStep(v)">{{ $t('next') }}</v-btn>
-                  </v-card-actions>
-                </template>
-              </CreateOrder>
-            </v-stepper-window-item>
+          <!-- Step 2: CreateOrder -->
+          <v-stepper-window-item :value="Steps.CreateOrder">
+            <CreateOrder>
+              <template v-slot:actions="{ v }">
+                <v-card-actions>
+                  <v-btn v-if="!consumerPicked" @click="step--">{{ $t('back') }}</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn @click="nextStep(v)">{{ $t('next') }}</v-btn>
+                </v-card-actions>
+              </template>
+            </CreateOrder>
+          </v-stepper-window-item>
 
-            <v-stepper-window-item :value="3">
-              <ExtraInfo>
-                <template v-slot:actions="{ v }">
-                  <v-card-actions>
-                    <v-btn @click="e1--">{{ $t('back') }}</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn @click="nextStep(v)">{{ $t('confirm') }}</v-btn>
-                  </v-card-actions>
-                </template>
-              </ExtraInfo>
-            </v-stepper-window-item>
-          </form>
+          <!-- Step 3: ExtraInfo -->
+          <v-stepper-window-item :value="Steps.ExtraInfo">
+            <ExtraInfo>
+              <template v-slot:actions="{ v }">
+                <v-card-actions>
+                  <v-btn @click="step--">{{ $t('back') }}</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn @click="nextStep(v)">{{ $t('confirm') }}</v-btn>
+                </v-card-actions>
+              </template>
+            </ExtraInfo>
+          </v-stepper-window-item>
+        </form>
       </v-stepper-window>
     </template>
   </v-stepper>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { cloneDeep } from 'lodash';
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { cloneDeep, isString } from 'lodash'
 
-import orders from '@/composables/localStore/useOrdersStore';
-import { upsertIndividuals } from '@/composables/localStore/useIndividualsStore';
-import { processOrder } from '@/composables/useStockManage';
+import orders from '@/composables/localStore/useOrdersStore'
+import { individuals, upsertIndividuals } from '@/composables/localStore/useIndividualsStore'
+import { processOrder } from '@/composables/useStockManage'
 
-import SelectConsumer from './CreateOrderStepper/SelectConsumer.vue';
-import CreateOrder from './CreateOrderStepper/CreateOrder.vue';
-import ExtraInfo from './CreateOrderStepper/ExtraInfo.vue';
+import SelectConsumer from './CreateOrderStepper/SelectConsumer.vue'
+import CreateOrder from './CreateOrderStepper/CreateOrder.vue'
+import ExtraInfo from './CreateOrderStepper/ExtraInfo.vue'
+import proformas from '@/composables/localStore/useProformaStore'
+import companies from '@/composables/localStore/useCompanyStore'
 
-import type { Validation } from '@vuelidate/core';
+import { form } from './CreateOrderStepper/state'
 
-import { form } from './CreateOrderStepper/state';
+import type { Validation } from '@vuelidate/core'
+import { DocumentType } from '@/models/models'
+import { computed } from 'vue'
 
-const dialog = defineModel<boolean>()
+enum Steps {
+  SelectConsumer = 1,
+  CreateOrder,
+  ExtraInfo
+}
+
 const emits = defineEmits(['success'])
 
-const steps = ref(3)
-const e1 = ref(1)
+const route = useRoute()
 
-const disabled = computed(() => e1.value === 1 ? 'prev' : e1.value === steps.value ? 'next' : undefined)
+const numericSteps: Steps[] = Object.values(Steps).filter(
+  (value) => typeof value === 'number'
+) as Steps[]
+const step = ref(Steps.SelectConsumer)
+
+const consumerPicked = computed(() => route.query.consumer)
+
+onMounted(() => {
+  const consumer = route.query.consumer
+
+  if (isString(consumer)) {
+    // Check if the consumer is a company
+    const company = companies.value.find((c) => c.id === consumer)
+    if (company) {
+      form.company = company.id
+    } else {
+      // Check if the consumer is an individual
+      const individual = individuals.value.find((i) => i.id === consumer)
+      if (individual) {
+        form.individual = individual
+      }
+    }
+
+    // Skip "SelectConsumer" step and start from step 2
+    step.value = Steps.CreateOrder
+  }
+})
 
 function nextStep(v: Validation) {
   v.$touch()
   if (!v.$invalid) {
-    if (e1.value === 3) {
+    if (step.value === 3) {
       const order = cloneDeep(form)
+      if (order.document_type === DocumentType.Proforma) {
+        proformas.value.unshift(order)
+        emits('success')
+        return
+      }
       orders.value.unshift(order)
-      if (order.individual)
-        upsertIndividuals(order.individual)
+      if (order.individual) upsertIndividuals(order.individual)
       processOrder(order)
       emits('success')
     }
-    e1.value++
+    step.value++
   }
 }
 </script>
--- src/views/OrdersView/CreateOrderStepper/SelectConsumer.vue
+++ src/views/OrdersView/CreateOrderStepper/SelectConsumer.vue
@@ -1,100 +1,100 @@
 <template>
   <div class="pa-4">
-    <v-radio-group v-model="consumerType" :error="!$v2.type.$pending && $v2.type.$error"  >
+    <v-radio-group v-model="consumerType">
       <v-radio :label="$t('company')" :value="ConsumerType.Company" />
       <v-radio :label="$t('individual')" :value="ConsumerType.Individual" />
     </v-radio-group>
 
-    <div v-if="consumerType == ConsumerType.Individual">  
-      <v-combobox 
-        :label="$t('name')" 
-        :items="individualsItems" 
+    <div v-if="consumerType == ConsumerType.Individual">
+      <v-combobox
+        :label="$t('name')"
+        :items="individualsItems"
         :error="!$v.individual.name.$pending && $v.individual.name.$error"
-        :error-messages="$v.individual.name.$errors.map(e => e as any)"
+        :error-messages="$v.individual.name.$errors.map((e) => e as any)"
         v-model="form.individual.name"
         :item-props="itemProps"
         @update:model-value="handleCustomerChange"
-        />
-      <v-text-field 
-        :label="$t('phone')" 
+      />
+      <v-text-field
+        :label="$t('phone')"
         :error="!$v.individual.phone.$pending && $v.individual.phone.$error"
         v-model="form.individual.phone"
-        />
+      />
     </div>
-    <v-select v-else-if="consumerType == ConsumerType.Company"
-      :label="$t('client')" 
-      :items="companiesItems" 
+    <v-select
+      v-else-if="consumerType == ConsumerType.Company"
+      :label="$t('client')"
+      :items="companiesItems"
       :error="!$v.company.$pending && $v.company.$error"
       item-title="name"
       item-value="id"
-      v-model="form.company" />
+      v-model="form.company"
+    />
   </div>
   <slot name="actions" :v="$v"></slot>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import { v4 as uuidv4 } from 'uuid';
-import { isString } from 'lodash';
-import useVuelidate from '@vuelidate/core';
-import { alpha, numeric, required, requiredIf } from '@vuelidate/validators';
+import { computed } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
+import { isString } from 'lodash'
+import useVuelidate from '@vuelidate/core'
+import { alpha, numeric, required, requiredIf } from '@vuelidate/validators'
 
-import { individuals } from '@/composables/localStore/useIndividualsStore';
-import companies from '@/composables/localStore/useCompanyStore';
+import { individuals } from '@/composables/localStore/useIndividualsStore'
+import companies from '@/composables/localStore/useCompanyStore'
 
-import { ConsumerType } from '@/models/models';
+import { ConsumerType } from '@/models/models'
 
-import { form, consumerType } from './state';
-
-const typeRules = {
-  type: { required } 
-};
-
-const $v2 = useVuelidate(typeRules, { type: consumerType });
+import { form, consumerType } from './state'
 
 const rules = {
   company: {
-    required: requiredIf(() => !form.individual.name && !form.individual.phone),  // required if individual is not filled
+    required: requiredIf(() => !form.individual.name && !form.individual.phone) // required if individual is not filled
   },
   individual: {
     name: {
-      required: requiredIf(() => !form.company),  // required if company is not filled
+      required: requiredIf(() => !form.company) // required if company is not filled
     },
     phone: {
-      required: requiredIf(() => !form.company),  // required if company is not filled
+      required: requiredIf(() => !form.company), // required if company is not filled
       numeric
     }
   }
 }
-const $v = useVuelidate(rules, form);
+const $v = useVuelidate(rules, form)
 
-const companiesItems = computed(() => companies.value.map(c => { 
-  return { id: c.id, name: c.name }
-}))
+const companiesItems = computed(() =>
+  companies.value.map((c) => {
+    return { id: c.id, name: c.name }
+  })
+)
 
-const individualsItems = computed(() => individuals.value.map(c => { 
-  return { id: c.id, name: c.name, phone: c.phone }
-}))
+const individualsItems = computed(() =>
+  individuals.value.map((c) => {
+    return { id: c.id, name: c.name, phone: c.phone }
+  })
+)
 
 const itemProps = (item: any) => {
-  if(isString(item)) return item
+  if (isString(item)) return item
   return {
     title: item.name,
-    subtitle: item.phone,
+    subtitle: item.phone
   }
 }
 
 function handleCustomerChange(item: any) {
-  form.individual.id = uuidv4();
-  form.individual.name = '';
-  form.individual.phone = '';
+  form.individual.id = uuidv4()
+  form.individual.name = ''
+  form.individual.phone = ''
 
-  const existingIndividual = individualsItems.value.find(individual => individual.id === item.id);
-  
+  const existingIndividual = individualsItems.value.find((individual) => individual.id === item.id)
+
   if (existingIndividual) {
-    form.individual = { ...existingIndividual };
-  } else if(isString(item)) {
-    form.individual.name = item;
+    form.individual = { ...existingIndividual }
+  } else if (isString(item)) {
+    form.individual.name = item
   }
 }
 </script>
