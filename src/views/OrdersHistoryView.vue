3319a2b41aa01a2345c0b59051d7331769136b37
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-11 16:03:08 +0100

    add date filter to history

--- src/views/OrdersHistoryView.vue
+++ src/views/OrdersHistoryView.vue
@@ -1,83 +1,111 @@
 <template>
-  <v-row>
-    <v-col sm="6" md="12">
-      <v-list
-        lines="three"
-        item-props
-        density="compact"
-      >
-        <template v-for="item in historyItems" :key="item.orderId || item.title">       
-           <div v-if="item.type === 'subheader'">       
-              <div class="pa-6 pb-2 text-medium-emphasis">{{ item.title }}</div>
-          </div>
-          <div v-else-if="item.orderId">
-            <v-list-item
-              :to="{ name: 'order', params: { order_id: item.orderId } }">
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-                <v-list-item-subtitle>
-                  <div v-html="item.subtitle"></div>
-                </v-list-item-subtitle>
-            </v-list-item>
-          </div>
+  <v-date-input
+    variant="underlined"
+    hide-details
+    v-model="dateRange"
+    :label="$t('date')"
+    max-width="368"
+    min-width="150"
+    multiple="range"
+  />
+  <v-row class="mt-10">
+    <v-col sm="12" md="4">
+      <v-list density="compact" class="overflow-y" style="max-height: 200px">
+        <template v-for="item in historyItems" :key="item.orderId || item.title">
+          <v-list-item>
+            <v-list-item-subtitle>
+              <div v-html="item.subtitle"></div>
+            </v-list-item-subtitle>
+          </v-list-item>
         </template>
       </v-list>
+      <div class="total text-end pa-4 font-italic" v-html="productSummary(allOrderlines)"></div>
     </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import { groupBy, sortBy } from 'lodash';
-import { format, isToday } from 'date-fns';
-import { useI18n } from 'vue-i18n';
+import { computed, ref } from 'vue'
+import { groupBy, sortBy, sum } from 'lodash'
+import { format, isSameDay } from 'date-fns'
+import { useI18n } from 'vue-i18n'
 
-import orders from '@/composables/localStore/useOrdersStore';
-import products from '@/composables/localStore/useProductStore';
-import companies from '@/composables/localStore/useCompanyStore';
+import orders from '@/composables/localStore/useOrdersStore'
+import products from '@/composables/localStore/useProductStore'
 
-type Item = {
-  title?: string;
-  subtitle?: string;
-  orderId?: string;
-  type?: string;
-  inset?: boolean;
-};
+import type { OrderLine } from '@/models/models'
 
 const { t } = useI18n()
 
+const dateRange = ref([])
+
+const filteredOrders = computed(() => {
+  return orders.value.filter((o) => {
+    const dateFilter = dateRange.value.length
+      ? dateRange.value.some((selectedDate) => isSameDay(o.date, selectedDate))
+      : true
+    return dateFilter
+  })
+})
+
 const historyItems = computed(() => {
-  let items = [] as Item[];
-  const groupedOrders = groupBy(
-    sortBy(orders.value, (t) => new Date(t.date)).reverse(),
-    (t) => new Date(t.date).toDateString()
-  );
-
-  for (const date in groupedOrders) {
-    const sortedItems = sortBy(groupedOrders[date].map(o => {
-      const orderLinesInfo = o.order_lines.map(ol => {
-        const info = { qte: ol.qte, product: getProduct(ol.product_id)?.name };
-        return `${info.qte}m ${info.product}`;
-      });
-
-      const consumerName = companies.value.find(e => e.id === o.company)?.name || o.individual?.name;
-      const intro = `<span class="text-primary">${consumerName}, ${format(o.date, 'p')}</span> &mdash;`;
-      const item = {
-        title: `${t('order')} ${t('N°')} ${o.index}`,
-        subtitle: `${intro} ${orderLinesInfo.join(' ,')}`,
-        orderId: o.id
-      };
-      return item;
-    }));
-
-    const firstItem = { type: 'subheader', title: isToday(date) ? t('today') : format(date, 'yyyy-MM-dd') };
-    items.push(firstItem);
-    items = items.concat(sortedItems);
+  let groupedSummary: any[] = []
+  for (const date in groupedOrders.value) {
+    const intro = `<span class="text-primary">${format(date, 'dd-MM-yyyy')}</span>&nbsp;&nbsp;`
+    const dateSummary = {
+      subtitle: `${intro} ${productSummary(allOrderlinesByDate.value[date])}`
+    }
+    groupedSummary.push(dateSummary)
   }
 
-  return items;
-});
+  return groupedSummary
+})
 
-const getProduct = (id: string) => products.value.find(e => e.id == id);
+const groupedOrders = computed(() => {
+  return groupBy(sortBy(filteredOrders.value, (t) => new Date(t.date)).reverse(), (t) =>
+    new Date(t.date).toDateString()
+  )
+})
+
+const allOrderlinesByDate = computed(() => {
+  const result: Record<string, OrderLine[]> = {}
+
+  Object.keys(groupedOrders.value).forEach((date) => {
+    const ordersForDate = groupedOrders.value[date]
+    result[date] = ordersForDate.reduce<OrderLine[]>((linesAcc, order) => {
+      return linesAcc.concat(order.order_lines)
+    }, [])
+  })
+
+  return result
+})
+
+const allOrderlines = computed(() => {
+  return Object.keys(groupedOrders.value).reduce<OrderLine[]>((acc: OrderLine[], date) => {
+    const ordersForDate = groupedOrders.value[date]
+    const orderLinesForDate = ordersForDate.reduce<OrderLine[]>(
+      (linesAcc, order) => linesAcc.concat(order.order_lines),
+      []
+    )
+    return acc.concat(orderLinesForDate)
+  }, [])
+})
+
+function productSummary(orderlines: OrderLine[]) {
+  let productSummaries: string[] = []
+  const orderlinesGrouped = groupBy(orderlines, (o) => o.product_id)
+  const totalPrice = sum(orderlines.map((o) => o.total_price))
+
+  for (const productId in orderlinesGrouped) {
+    const product = getProduct(productId)
+    const totalQte = sum(orderlinesGrouped[productId].map((o) => o.qte))
+    productSummaries.push(`${totalQte}m ${product?.name}`)
+  }
+
+  return `${productSummaries.join(', ')} &mdash; ${totalPrice} ${t('DA')}`
+}
+
+const getProduct = (id: string) => products.value.find((e) => e.id == id)
 </script>
 
 <style scoped>
