458c5f7bf507863d33206a385fbb3072b801f9df
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-11 15:57:49 +0100

    add menu to cards & delete button

--- src/views/ClientsView/ClientCard.vue
+++ src/views/ClientsView/ClientCard.vue
@@ -5,17 +5,36 @@
     :prepend-icon="consumerType == ConsumerType.Company ? mdiDomain : mdiAccount"
     :title="client?.name"
   >
-    <v-card-subtitle style="line-height: 150%;">
+    <v-card-subtitle style="line-height: 150%">
       <div v-if="'address' in client">{{ $t('address') }}: {{ client?.address }}</div>
-      <div>{{ $t('phone') }}: <a :href="`tel:${client?.phone}`">{{ client?.phone }}</a> </div>
+      <div>
+        {{ $t('phone') }}: <a :href="`tel:${client?.phone}`">{{ client?.phone }}</a>
+      </div>
     </v-card-subtitle>
 
     <v-card-actions class="ma-0 pa-0">
       <v-spacer></v-spacer>
-      <v-btn v-if="client.id"
+      <v-btn
+        class="my-5"
         size="small"
-        :append-icon="mdiHistory" 
-         :to="{ name:'client-history', params: { client_id: client.id }}">
+        variant="plain"
+        :append-icon="mdiPlus"
+        :to="{
+          name: 'create-order',
+          query: {
+            consumer: client.id
+          }
+        }"
+      >
+        {{ $t('add-order') }}
+      </v-btn>
+      <v-btn
+        v-if="client.id"
+        size="small"
+        :append-icon="mdiHistory"
+        variant="plain"
+        :to="{ name: 'client-history', params: { client_id: client.id } }"
+      >
         {{ $t('history') }}
       </v-btn>
     </v-card-actions>
@@ -23,34 +42,45 @@
     <template v-slot:append>
       <v-menu>
         <template v-slot:activator="{ props }">
-          <v-btn class="ml-auto" 
-            v-bind="props" 
-            variant="text"
-            :icon="mdiDotsVertical" />
-          </template>
+          <v-btn class="ml-auto" v-bind="props" variant="text" :icon="mdiDotsVertical" />
+        </template>
         <v-list density="compact">
-          <v-list-item density="compact" @click="deleteClient">
-            <v-list-item-title>Supprimer</v-list-item-title>
+          <v-list-item density="compact" @click="deleteDialog = true">
+            <v-list-item-title>{{ $t('delete') }}</v-list-item-title>
           </v-list-item>
         </v-list>
-      </v-menu> 
+      </v-menu>
     </template>
+    <DeleteItemModal v-model="deleteDialog" @confirm="deleteClient" @close="deleteDialog = false" />
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import { mdiAccount, mdiDomain, mdiDotsVertical, mdiHistory } from '@mdi/js';
+import { computed, ref } from 'vue'
+import { mdiAccount, mdiDomain, mdiDotsVertical, mdiHistory, mdiPlus } from '@mdi/js'
 
-import { ConsumerType, type Company, type Individual } from '@/models/models';
+import companies from '@/composables/localStore/useCompanyStore'
+import { individuals } from '@/composables/localStore/useIndividualsStore'
+
+import DeleteItemModal from '../OrdersView/DeleteItemModal.vue'
+
+import { ConsumerType, type Company, type Individual } from '@/models/models'
 
 const client = defineModel<Company | Individual>()
 
-const consumerType = computed(() => {
-  return client.value && 'rc' in client.value
-    ? ConsumerType.Company
-    : ConsumerType.Individual;
-});
+const deleteDialog = ref(false)
 
-function deleteClient() {}
+const consumerType = computed(() => {
+  return client.value && 'rc' in client.value ? ConsumerType.Company : ConsumerType.Individual
+})
+
+function deleteClient() {
+  if (consumerType.value == ConsumerType.Company && client.value) {
+    const index = companies.value.indexOf(client.value as Company)
+    companies.value.splice(index, 1)
+  } else if (consumerType.value == ConsumerType.Individual && client.value) {
+    const index = individuals.value.indexOf(client.value as Individual)
+    individuals.value.splice(index, 1)
+  }
+}
 </script>
--- src/views/WarehouseView/ProductCard.vue
+++ src/views/WarehouseView/ProductCard.vue
@@ -1,29 +1,63 @@
 <!-- eslint-disable vue/no-v-text-v-html-on-component -->
 <template>
-  <v-card 
-    class="pa-4" 
-    hover 
-    :to="{ name: 'product', params: { product_id: product?.id }}"> 
-    <v-card-title v-text="product?.name" />
+  <v-card
+    class="pb-4"
+    hover
+    :title="product?.name"
+    :to="{ name: 'product', params: { product_id: product?.id } }"
+  >
     <v-card-subtitle class="text-caption">
-      <span class="font-weight-bold">{{ $t('code') }}:</span> 
-      <span>&nbsp;{{ product?.code }}</span><br>
-      <span class="font-weight-bold">{{ $t('quantity') }}:</span> 
-      <span>&nbsp;{{ product?.qte }}</span><br>
-      <span class="font-weight-bold">{{ $t('U.P') }}:</span> 
+      <span class="font-weight-bold">{{ $t('code') }}:</span>
+      <span>&nbsp;{{ product?.code }}</span
+      ><br />
+      <span class="font-weight-bold">{{ $t('quantity') }}:</span>
+      <span>&nbsp;{{ product?.qte }}</span
+      ><br />
+      <span class="font-weight-bold">{{ $t('U.P') }}:</span>
       <span>&nbsp;{{ product?.price }} DA</span>
     </v-card-subtitle>
+
+    <template v-slot:append>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn
+            v-bind="props"
+            @click.prevent
+            variant="text"
+            size="small"
+            :icon="mdiDotsVertical"
+          />
+        </template>
+        <v-list density="compact">
+          <v-list-item density="compact" @click="deleteDialog = true">
+            <v-list-item-title>{{ $t('delete') }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+    <DeleteItemModal
+      v-model="deleteDialog"
+      @confirm="deleteProduct"
+      @close="deleteDialog = false"
+    />
   </v-card>
 </template>
 <script setup lang="ts">
-import products from '@/composables/localStore/useProductStore';
+import { ref } from 'vue'
+import { mdiDotsVertical } from '@mdi/js'
 
-import type { Product } from '@/models/models';
+import products from '@/composables/localStore/useProductStore'
+
+import DeleteItemModal from '../OrdersView/DeleteItemModal.vue'
+
+import type { Product } from '@/models/models'
 
 const product = defineModel<Product>()
 
+const deleteDialog = ref(false)
+
 function deleteProduct() {
-  const index = products.value.indexOf(product.value!);
-  products.value.splice(index, 1);
+  const index = products.value.indexOf(product.value!)
+  products.value.splice(index, 1)
 }
 </script>
