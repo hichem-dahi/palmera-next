5de303959b8461efcff6d07c3ddcafb249ccd57c
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-12 15:38:34 +0100

    add doc type, title to orderCard

--- src/views/OrdersView/OrderCard.vue
+++ src/views/OrdersView/OrderCard.vue
@@ -1,50 +1,68 @@
 <template>
-  <v-card 
-    v-if="order" 
-    color="grey-lighten-5" 
-    class="pa-4" 
+  <v-card
+    v-if="order"
+    color="grey-lighten-5"
+    class="pa-4"
     hover
-    variant="elevated" 
-    :to="{ name: 'order', params: {order_id: order.id}}" >
+    variant="elevated"
+    :to="{ name: 'order', params: { order_id: order.id } }"
+  >
     <v-card-title>{{ $t('order') }} {{ $t('N°') }} {{ order.index }}</v-card-title>
-
-    <v-card-subtitle class="order-info">
-      <div class="d-flex">
-        <div class="font-weight-bold">{{ $t('client') }}:</div> 
-        <div>&nbsp;{{ consumerName }}</div>
+    <v-card-text class="order-info py-0">
+      <div>
+        <span class="font-weight-bold">{{ $t('client') }}:</span>
+        <span>&nbsp;{{ consumerName }}</span>
       </div>
-      <div class="d-flex">
-        <div class="font-weight-bold">{{ $t('date') }}:</div> 
-        <div>&nbsp;{{ format(order.date, 'yyyy-MM-dd' ) }}</div>
+      <div>
+        <span class="font-weight-bold">{{ $t('date') }}:</span>
+        <span>&nbsp;{{ format(order.date, 'yyyy-MM-dd') }}</span>
       </div>
-      <div class="d-flex">
-        <div class="font-weight-bold">{{ $t('total') }}:</div> 
-        <div>&nbsp;{{ order.total_price }}DA</div>
+      <div>
+        <span class="font-weight-bold">{{ $t('total') }}:</span>
+        <span>&nbsp;{{ order.total_price }} {{ $t('DA') }}</span>
       </div>
-      <div class="d-flex">
-        <div class="font-weight-bold"> {{$t('remaining')}}:</div> 
-        <div class="text-red">&nbsp;{{ order.total_price - order.paid_price }}DA</div>
+      <div>
+        <span class="font-weight-bold">{{ $t('remaining') }}:</span>
+        <span class="text-red"
+          >&nbsp;{{ order.total_price - order.paid_price }} {{ $t('DA') }}</span
+        >
       </div>
-    </v-card-subtitle>
+    </v-card-text>
+    <v-card-actions class="py-0">
+      <v-spacer></v-spacer>
+      <v-btn variant="text" size="x-small">
+        {{ docTitle }}
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import { format } from 'date-fns';
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { format } from 'date-fns'
 
-import companies from '@/composables/localStore/useCompanyStore';
+import companies from '@/composables/localStore/useCompanyStore'
 
-import { type Order } from '@/models/models';
+import { DocumentType, type Order } from '@/models/models'
+import { kebabCase } from 'lodash'
 
-const order = defineModel<Order>('order')
+const { t } = useI18n()
+const order = defineModel<Order>('order', { required: true })
 
-const consumerName = computed(() => 
-  companies.value.find(e => e.id === order.value?.company)?.name || 
-  order.value?.individual?.name)
+const docTitle = computed(
+  () =>
+    `${t(kebabCase(DocumentType[order.value.document_type]))} ${t('N°')} ${order.value.docIndex}`
+)
+const consumerName = computed(
+  () =>
+    companies.value.find((e) => e.id === order.value?.company)?.name ||
+    order.value?.individual?.name
+)
 </script>
 
 <style>
 .order-info {
   font-size: 0.8rem;
-}</style>
+}
+</style>
