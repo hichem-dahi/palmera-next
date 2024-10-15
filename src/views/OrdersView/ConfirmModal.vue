9a8a6d54e8ee659299239a339311cc82586f4395
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-15 13:45:37 +0100

    add confirm modal

---
+++ src/views/OrdersView/ConfirmModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card :prepend-icon="mdiAlert">
+      <template v-slot:title> Avertissement </template>
+      <template v-slot:text>
+        <div>Vous ne pourrez plus modifier cette commande après cela. Voulez-vous continuer ?</div>
+      </template>
+      <template v-slot:prepend>
+        <v-icon color="yellow" size="x-large" />
+      </template>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue-darken-1" variant="text" @click="emits('close')">
+          {{ $t('cancel') }}
+        </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="emits('confirm')">
+          {{ $t('confirm') }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { mdiAlert } from '@mdi/js'
+
+const dialog = defineModel<boolean>()
+const emits = defineEmits(['close', 'confirm'])
+</script>
+<style scoped>
+.v-card {
+  white-space: normal;
+}
+</style>
