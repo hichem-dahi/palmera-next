e0160bd0069cadd5868cd78f7a6ca7275e24184e
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-15 13:43:04 +0100

    restyle delete item modal

--- src/views/OrdersView/DeleteItemModal.vue
+++ src/views/OrdersView/DeleteItemModal.vue
@@ -1,18 +1,23 @@
 <template>
   <v-dialog v-model="dialog" max-width="500px">
-    <v-card :prepend-icon="mdiDeleteOutline" title="Are you sure you want to delete this item?">
+    <v-card :prepend-icon="mdiDeleteOutline">
+      <template v-slot:title> Avertissement </template>
+
+      <template v-slot:text>
+        <div class="text-center">Are you sure you want to delete this item?</div>
+      </template>
       <template v-slot:prepend>
         <v-icon color="red" size="x-large"></v-icon>
       </template>
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue-darken-1" variant="text" @click="emits('close')">{{
-          $t('cancel')
-        }}</v-btn>
-        <v-btn color="blue-darken-1" variant="text" @click="emits('confirm')">{{
-          $t('confirm')
-        }}</v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="emits('close')">
+          {{ $t('cancel') }}
+        </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="emits('confirm')">
+          {{ $t('confirm') }}
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
