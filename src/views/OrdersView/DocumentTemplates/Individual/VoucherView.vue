b5f4962b60c22a0367c38dd389b78678fe62d693
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-17 12:21:20 +0100

    add 3 photocopies for Voucher

--- src/views/OrdersView/DocumentTemplates/Individual/VoucherView.vue
+++ src/views/OrdersView/DocumentTemplates/Individual/VoucherView.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="voucher-wrapper">
-    <div v-if="order" class="voucher">
-      <h2>{{ self.company?.name }}</h2>
-      <div>{{ self.company?.activity }}</div>
-      <div>{{ self.company?.phone }}</div>
+  <div class="voucher-wrapper" v-if="order">
+    <div class="voucher" v-for="i in 3" :key="i">
+      <div class="d-flex align-center">
+        <div style="width: 33%">
+          <h3>{{ self.company?.name }}</h3>
+          <div>{{ self.company?.activity }}</div>
+          <div>{{ self.company?.phone }}</div>
+        </div>
+        <h3 class="type">
+          {{ title }} N°: {{ padStart(order.docIndex?.toString(), 4, '0') }}/2024
+        </h3>
+      </div>
+      <div class="d-flex align-start justify-space-between mt-5">
+        <div>
+          <div v-for="(value, key) in individualInfo" :key="key">
+            <span v-if="value">{{ key }}: {{ value }}</span>
+          </div>
+        </div>
+        <div class="delivery-info" v-if="($route.query.type as any) == DocumentType.DeliveryNote">
+          <div v-for="(value, key) in deliveryInfo" :key="key">
+            <span>
+              <b>{{ key }}:</b> {{ value }}
+            </span>
+          </div>
+        </div>
+      </div>
 
-      <h3 class="type">{{ title }} N°: {{ padStart(order.docIndex?.toString(), 4, '0') }}/2024</h3>
-      <span>date: {{ format(order.date, 'dd-MM-yyyy') }}</span
-      ><br />
-      <span>client: {{ order.individual?.name }}</span
-      ><br />
-      <span>N°tel: {{ order.individual?.phone }}</span
-      ><br />
       <table cellpadding="10" cellspacing="0" width="100%">
         <thead>
           <tr>
@@ -43,12 +57,7 @@
         </tfoot>
       </table>
       <div class="total-words">{{ totalWords }}</div>
-      <div class="delivery-info" v-if="($route.query.type as any) == DocumentType.DeliveryNote">
-        <div v-for="(value, key) in deliveryInfo" :key="key">
-          <span class="font-weight-bold">{{ key }}:</span>
-          <span>&nbsp;{{ value }}</span>
-        </div>
-      </div>
+      <v-divider :class="{ 'mb-5': i !== 3 }" />
     </div>
     <div class="actions no-print">
       <v-btn class="mr-5" @click="print()">print</v-btn>
@@ -86,6 +95,16 @@ const totalWords = computed(() => {
     words += ' virgule ' + n2words(Math.floor(decimalPart * 10), { lang: 'fr' })
   }
   return `${words} dinars alg`
+})
+
+const individualInfo = computed(() => {
+  const individualInfoKeys = ['date', 'name', 'N°tel']
+  let individual = {
+    date: format(order.value?.date || '', 'dd-MM-yyyy'),
+    name: order.value?.individual?.name,
+    'N°tel': order.value?.individual?.phone
+  }
+  return pick(individual, individualInfoKeys)
 })
 
 const deliveryInfo = computed(() => {
@@ -156,11 +175,10 @@ function downloadInvoice() {
 <style>
 .voucher {
   text-transform: capitalize;
-  overflow-y: auto; /* Add scroll to the wrapper if content exceeds the height */
   white-space: nowrap;
   margin: auto;
-  transform: scale(0.7); /* Scale down the element */
   transform-origin: top; /* Ensure scaling happens from the top */
+  max-height: calc(100% / 3);
 
   @media (min-width: 1024px) {
     /* Target screens larger than 1024px (typical desktop size) */
@@ -168,14 +186,14 @@ function downloadInvoice() {
   }
 
   table {
-    margin-top: 25px;
+    margin-top: 8px;
   }
 
   th,
   td {
     border: 1px solid black;
     border-collapse: collapse; /* Ensures borders don't double up */
-    padding: 4px 8px;
+    padding: 2px 4px;
   }
 
   .no-border {
@@ -183,10 +201,8 @@ function downloadInvoice() {
   }
 
   .type {
-    margin-top: 5px;
     text-decoration: underline;
     text-align: center;
-    padding: 1rem;
   }
 
   .total-words {
@@ -197,17 +213,23 @@ function downloadInvoice() {
 
   .delivery-info {
     max-width: min-content;
-    font-size: 12px;
+    font-size: 0.6rem;
     white-space: nowrap;
-    padding: 1rem;
-    border: 1px solid slategrey;
+    padding: 0.5rem 1rem;
   }
 }
 
 @media print {
+  .voucher-wrapper {
+    max-height: 100vh; /* Ensure the vouchers fit on one page */
+  }
+
   .voucher {
+    font-size: small;
     max-width: none;
-    transform: none;
+    margin: 0;
+    page-break-inside: avoid; /* Prevent breaking inside voucher */
+    max-height: calc(100vh / 3);
   }
 
   .no-print {
