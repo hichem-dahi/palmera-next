6bcc14e5beda09d229c340652dda302f49a35ce3
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-20 08:44:20 +0100

    add auth lofgic

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,47 @@
 <template>
   <router-view></router-view>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+import { supabase } from './supabase/supabase'
+
+import { useGetProfileApi } from './composables/api/auth/useGetProfileApi'
+
+import self from './composables/localStore/useSelf'
+
+const route = useRoute()
+const router = useRouter()
+
+const getProfileApi = useGetProfileApi()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    if (data.session) self.value.session = data.session
+  })
+
+  supabase.auth.onAuthStateChange(async (_, _session) => {
+    if (_session) {
+      self.value.session = _session
+      if (route.path === '/') router.push('/')
+    } else {
+      router.push('/auth')
+    }
+  })
+})
+
+watch(
+  () => self.value.session,
+  (newSession) => {
+    if (newSession) getProfileApi.userId.value = newSession.user.id
+  }
+)
+
+watch(
+  () => getProfileApi.data.value,
+  (profileData) => {
+    if (profileData) self.value.user = profileData
+  }
+)
+</script>
--- src/composables/localStore/useSelf.ts
+++ src/composables/localStore/useSelf.ts
@@ -1,10 +1,19 @@
-import type { Company } from '@/models/models'
 import { useLocalStorage } from '@vueuse/core'
 
+import type { Session } from '@supabase/supabase-js'
+
+import type { Company } from '@/models/models'
+
 interface Self {
+  user?: any
+  session?: Session
   company?: Company
 }
 
-const self = useLocalStorage<Self>('self', { company: undefined })
+const self = useLocalStorage<Self>('self', {
+  user: undefined,
+  session: undefined,
+  company: undefined
+})
 
 export default self
