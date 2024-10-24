36a9ae003eb65cc400b6d776e394454e78517da6
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-24 18:56:06 +0100

    refactor: add DocumentButtons component

---
+++ src/views/OrderView/DocumentButtons.vue
@@ -0,0 +1,36 @@
+<template>
+  <template v-for="(docType, index) in documentTypes" :key="index">
+    <v-btn
+      variant="text"
+      :prepend-icon="docType.icon"
+      :disabled="!isConfirmable"
+      @click="goDocPage"
+      target="_blank"
+      :text="$t(docType.label)"
+      v-if="order?.document_type === docType.type"
+    />
+  </template>
+</template>
+
+<script setup lang="ts">
+import { mdiInvoice, mdiReceiptText, mdiTruckCheck, mdiNote } from '@mdi/js'
+import { DocumentType } from '@/models/models'
+
+const props = defineProps({
+  order: Object,
+  isConfirmable: Boolean
+})
+
+const emit = defineEmits(['go-doc-page'])
+
+const documentTypes = [
+  { type: DocumentType.DeliveryNote, icon: mdiTruckCheck, label: 'delivery-note' },
+  { type: DocumentType.Invoice, icon: mdiReceiptText, label: 'invoice' },
+  { type: DocumentType.Voucher, icon: mdiInvoice, label: 'voucher' },
+  { type: DocumentType.Proforma, icon: mdiNote, label: 'proforma' }
+]
+
+function goDocPage() {
+  emit('go-doc-page')
+}
+</script>
