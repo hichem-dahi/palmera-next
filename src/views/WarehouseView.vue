b101de800f6d56104a311746994a705e06400b85
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-25 15:03:08 +0100

    feat: add edit product logic and refactor form

--- src/views/WarehouseView.vue
+++ src/views/WarehouseView.vue
@@ -2,8 +2,12 @@
   <v-btn class="my-5" variant="tonal" size="small" :append-icon="mdiPlus" @click="dialog = true">
     {{ $t('add-product') }}
   </v-btn>
-  <v-dialog v-model="dialog" max-width="400px">
-    <CreateProduct v-model="dialog" />
+  <v-dialog max-width="400px" v-model="dialog">
+    <ProductForm>
+      <template v-slot:actions="{ form, v }">
+        <v-btn block @click="submitForm(form, v)">{{ $t('add') }}</v-btn>
+      </template>
+    </ProductForm>
   </v-dialog>
   <v-row>
     <v-col v-for="(_, i) in products" :key="i" sm="12" md="3">
@@ -18,8 +22,19 @@ import { mdiPlus } from '@mdi/js'
 
 import products from '@/composables/localStore/useProductStore'
 
-import CreateProduct from '@/views/WarehouseView/CreateProduct.vue'
+import ProductForm from '@/views/WarehouseView/ProductForm.vue'
 import ProductCard from '@/views/WarehouseView/ProductCard.vue'
 
+import type { Product } from '@/models/models'
+import type { Validation } from '@vuelidate/core'
+
 const dialog = ref(false)
+
+function submitForm(form: Product, v: Validation<Product>) {
+  v.$touch()
+  if (!v.$invalid) {
+    products.value.push(form)
+    dialog.value = false
+  }
+}
 </script>
