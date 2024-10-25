eb92c82aeaaddd03fc9c91caa0986c24601edab2
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-25 15:02:35 +0100

    refactor: rename to ProductForm

--- src/views/WarehouseView/ProductForm.vue
+++ src/views/WarehouseView/ProductForm.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-sheet class="pa-4">
+  <v-card class="pa-4">
     <v-text-field
       :label="$t('code')"
-      v-model="form.code"
+      v-model="proxyForm.code"
       :error-messages="!$v.code.$pending && $v.code.$error ? $t('Code is required') : ''"
       @blur="$v.code.$touch()"
     />
     <v-text-field
       :label="$t('name')"
-      v-model="form.name"
+      v-model="proxyForm.name"
       :error-messages="!$v.name.$pending && $v.name.$error ? $t('Name is required') : ''"
       @blur="$v.name.$touch()"
     />
     <v-text-field
       :label="$t('quantity')"
-      v-model.number="form.qte"
+      v-model="proxyForm.qte"
       type="number"
       :error-messages="
         !$v.qte.$pending && $v.qte.$error ? $t('Qunatity must be greater than zero') : ''
@@ -24,37 +24,37 @@
 
     <v-text-field
       :label="$t('price')"
-      v-model.number="form.price"
+      v-model="proxyForm.price"
       type="number"
       :error-messages="
         !$v.price.$pending && $v.price.$error ? $t('Price must be greater than zero') : ''
       "
       @blur="$v.price.$touch()"
     />
-
-    <v-btn block @click="submitForm">{{ $t('add') }}</v-btn>
-  </v-sheet>
+    <slot name="actions" :form="proxyForm" :v="$v"></slot>
+  </v-card>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { ref, toRef } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
+import { cloneDeep } from 'lodash'
 import useVuelidate from '@vuelidate/core'
 import { required, minValue, numeric } from '@vuelidate/validators'
 
-import products from '@/composables/localStore/useProductStore'
-
 import type { Product } from '@/models/models'
 
-const isOpen = defineModel()
+const props = defineProps<{ form?: Product }>()
 
-const form = reactive<Product>({
+const proxyForm = ref<Product>({
   id: uuidv4(),
   code: '',
   name: '',
   qte: null,
   price: null
 })
+
+if (props.form) proxyForm.value = cloneDeep(props.form)
 
 const rules = {
   id: { required },
@@ -64,13 +64,8 @@ const rules = {
   price: { required, numeric, minValue: minValue(1) }
 }
 
-const $v = useVuelidate(rules, form)
-
-async function submitForm() {
-  $v.value.$touch()
-  if (!$v.value.$invalid) {
-    products.value.push(form)
-    isOpen.value = false
-  }
-}
+const $v = useVuelidate(
+  rules,
+  toRef(() => proxyForm.value)
+)
 </script>
