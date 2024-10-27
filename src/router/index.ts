ad601b34f38f9b4eaf68b6e87c0605456154bd4c
Hichem Dahi <hichemdahi57@gmail.com>
2024-10-27 11:27:37 +0100

    feat: add AuthView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import AuthView from '@/views/AuthView.vue'
 import HomeView from '@/views/HomeView.vue'
 import WarehouseView from '@/views/WarehouseView.vue'
 import PersonnelView from '@/views/PersonnelView.vue'
