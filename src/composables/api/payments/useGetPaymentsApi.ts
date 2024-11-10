5c3d9fdd88902da244273c47455f34231deeb81a
Hichem <hichemdahi57@gmail.com>
2024-11-10 13:10:28 +0100

    feat: add payment apis

---
+++ src/composables/api/payments/useGetPaymentsApi.ts
@@ -0,0 +1,24 @@
+import { computed, ref } from 'vue'
+import { useAsyncState } from '@vueuse/core'
+
+import { supabase } from '@/supabase/supabase'
+
+export function useGetPaymentsApi() {
+  const orderId = ref<string>()
+
+  const query = async () => {
+    if (orderId.value) {
+      return supabase.from('payments').select().eq('order_id', orderId.value)
+    } else {
+      throw new Error('Form is null or incomplete')
+    }
+  }
+
+  const q = useAsyncState(query, undefined) // Invoke query properly
+
+  const data = computed(() => q.state.value?.data)
+  const error = computed(() => q.state.value?.error)
+  const isSuccess = computed(() => q.isReady.value && !error.value)
+
+  return { ...q, data, error, isSuccess, orderId }
+}
---
+++ src/composables/api/payments/useInsertPaymentApi.ts
@@ -0,0 +1,26 @@
+import { computed, ref } from 'vue'
+import { useAsyncState } from '@vueuse/core'
+
+import { supabase } from '@/supabase/supabase'
+
+import type { TablesInsert } from '@/types/database.types'
+
+export function useInsertPaymentsApi() {
+  const form = ref<TablesInsert<'payments'>>() // Use ref to make it reactive
+
+  const query = async () => {
+    if (form.value) {
+      return supabase.from('payments').insert(form.value).select().single()
+    } else {
+      throw new Error('Form is null or incomplete')
+    }
+  }
+
+  const q = useAsyncState(query, undefined) // Invoke query properly
+
+  const data = computed(() => q.state.value?.data)
+  const error = computed(() => q.state.value?.error)
+  const isSuccess = computed(() => q.isReady.value && !error.value)
+
+  return { ...q, data, error, isSuccess, form }
+}
