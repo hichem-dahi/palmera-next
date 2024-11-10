622d93ceeb1d1b2c193472b32b918730f029df33
Hichem <hichemdahi57@gmail.com>
2024-11-10 12:59:45 +0100

    refactor: useGetProductApi return params

--- src/composables/api/products/useGetProductApi.ts
+++ src/composables/api/products/useGetProductApi.ts
@@ -19,7 +19,7 @@ export function useGetProductApi() {
 
   const data = computed(() => q.state.value?.data)
   const error = computed(() => q.state.value?.error)
-  const isSuccess = computed(() => q.isReady.value && q.state.value?.statusText === 'OK')
+  const isSuccess = computed(() => q.isReady.value && !error.value)
 
-  return { ...q, data, error, isSuccess }
+  return { ...q, data, error, isSuccess, params }
 }
