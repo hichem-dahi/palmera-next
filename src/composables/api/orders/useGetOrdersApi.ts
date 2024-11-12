6ae32da7685dc5eac3c105b2b916c5a6954f793b
Hichem <hichemdahi57@gmail.com>
2024-11-12 14:13:48 +0100

    feat: add get orders filers

--- src/composables/api/orders/useGetOrdersApi.ts
+++ src/composables/api/orders/useGetOrdersApi.ts
@@ -1,22 +1,45 @@
-import { computed } from 'vue'
+import { computed, reactive } from 'vue'
 import { useAsyncState } from '@vueuse/core'
 
 import { supabase } from '@/supabase/supabase'
+import type { OrderStatus } from '@/models/models'
 
 export function useGetOrdersApi() {
-  const query = async () =>
-    supabase.from('orders').select(`
-    *,
-    client:organizations!orders_client_id_fkey (*),    
-    delivery:deliveries (*),
-    individual:individuals (*)
-  `)
+  const params = reactive({
+    dateRange: [] as [string, string] | [], // Array with [startDate, endDate]
+    status: null as OrderStatus | null
+  })
 
-  const q = useAsyncState(query, undefined, { immediate: true }) // Invoke query properly
+  const query = async () => {
+    const queryBuilder = supabase.from('orders').select(
+      `
+        *,
+        client:organizations!orders_client_id_fkey (*),   
+        order_lines:order_lines (
+          *,
+          product:products (*)
+        ),  
+        delivery:deliveries (*),
+        individual:individuals (*)
+      `
+    )
+
+    if (params.dateRange.length === 2) {
+      queryBuilder.gte('date', params.dateRange[0]).lte('date', params.dateRange[1])
+    }
+
+    if (params.status !== null) {
+      queryBuilder.eq('status', params.status)
+    }
+
+    return queryBuilder
+  }
+
+  const q = useAsyncState(query, undefined, { immediate: false }) // Invoke query properly
 
   const data = computed(() => q.state.value?.data)
   const error = computed(() => q.state.value?.error)
-  const isSuccess = computed(() => q.isReady.value && q.state.value?.statusText === 'OK')
+  const isSuccess = computed(() => q.isReady.value && !error.value)
 
-  return { ...q, data, error, isSuccess }
+  return { ...q, data, error, isSuccess, params }
 }
