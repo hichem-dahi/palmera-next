c9ae9bcfac996984fb6d69055d43a597915da09c
Hichem <hichemdahi57@gmail.com>
2024-11-12 14:09:42 +0100

    feat: add individuals apis

---
+++ src/composables/api/individuals/useGetIndividualApi.ts
@@ -0,0 +1,24 @@
+import { computed, ref } from 'vue'
+import { useAsyncState } from '@vueuse/core'
+
+import { supabase } from '@/supabase/supabase'
+
+export function useGetIndividualApi() {
+  const individualId = ref<string>()
+
+  const query = async () => {
+    if (individualId.value) {
+      return supabase.from('individuals').select().eq('id', individualId.value).single()
+    } else {
+      throw new Error('Form is null or incomplete')
+    }
+  }
+
+  const q = useAsyncState(query, undefined) // Invoke query properly
+
+  const data = computed(() => q.state.value?.data)
+  const error = computed(() => q.state.value?.error)
+  const isSuccess = computed(() => q.isReady.value && !error.value)
+
+  return { ...q, data, error, isSuccess, individualId }
+}
