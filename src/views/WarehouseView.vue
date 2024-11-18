f2af63e20f2b1cce4dcbe1e515a817846b3a430c
Hichem <hichemdahi57@gmail.com>
2024-11-18 23:22:36 +0100

    feat: add products filters (WarehouseView)

--- src/views/WarehouseView.vue
+++ src/views/WarehouseView.vue
@@ -1,25 +1,31 @@
 <template>
-  <v-btn class="my-5" variant="tonal" size="small" :append-icon="mdiPlus" @click="dialog = true">
-    {{ $t('add-product') }}
-  </v-btn>
-  <v-dialog max-width="400px" v-model="dialog">
-    <ProductForm v-model="form">
-      <template v-slot:actions>
-        <v-btn block :loading="insertProductApi.isLoading.value" @click="submitForm()">
-          {{ $t('add') }}
-        </v-btn>
-      </template>
-    </ProductForm>
-  </v-dialog>
-  <v-row>
-    <v-col v-for="(_, i) in products" :key="i" sm="12" md="3">
-      <ProductCard v-model="products[i]" />
-    </v-col>
-  </v-row>
+  <div class="d-flex align-start flex-wrap ga-8 pa-4">
+    <v-btn class="my-5" variant="tonal" size="small" :append-icon="mdiPlus" @click="dialog = true">
+      {{ $t('add-product') }}
+    </v-btn>
+    <v-dialog max-width="400px" v-model="dialog">
+      <ProductForm v-model="form">
+        <template v-slot:actions>
+          <v-btn block :loading="insertProductApi.isLoading.value" @click="submitForm()">
+            {{ $t('add') }}
+          </v-btn>
+        </template>
+      </ProductForm>
+    </v-dialog>
+    <v-divider v-if="!$vuetify.display.mobile" vertical />
+    <FilterBar v-model="filters" />
+  </div>
+  <v-container>
+    <v-row>
+      <v-col v-for="(_, i) in filteredProducts" :key="i" sm="12" md="3">
+        <ProductCard v-model="filteredProducts[i]" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
 import { mdiPlus } from '@mdi/js'
 
@@ -30,6 +36,7 @@ import { useGetProductsApi } from '@/composables/api/products/useGetProductsApi'
 
 import ProductForm from '@/views/WarehouseView/ProductForm.vue'
 import ProductCard from '@/views/WarehouseView/ProductCard.vue'
+import FilterBar from './WarehouseView/FilterBar.vue'
 
 const $v = useVuelidate()
 
@@ -38,16 +45,31 @@ const getProductsApi = useGetProductsApi()
 
 const dialog = ref(false)
 
+const filters = reactive({
+  name: null,
+  barcode: null
+})
+
 const form = ref({
   code: '',
   name: '',
   organization_id: self.value.user?.organization_id || '',
   qte: 0,
   price: 0,
-  cost_price: null as number | null
+  cost_price: null as number | null,
+  bar_code: null as number | null
 })
 
 const products = computed(() => getProductsApi.data.value || [])
+
+const filteredProducts = computed(
+  () =>
+    products.value.filter((o) => {
+      const name = filters.name ? o.name.includes(filters.name) : true
+      const barcode = filters.barcode && o.bar_code ? o.bar_code === filters.barcode : true
+      return name && barcode
+    }) || []
+)
 
 function submitForm() {
   $v.value.$touch()
---
+++ src/views/WarehouseView/FilterBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="filters d-flex align-end ga-6">
+    <v-text-field
+      variant="underlined"
+      hide-details
+      v-model="model.name"
+      max-width="368"
+      min-width="100"
+      :label="$t('name')"
+      clearable
+    />
+    <v-btn variant="text" :append-icon="mdiBarcodeScan" @click="showScanner = true">{{
+      $t('scan')
+    }}</v-btn>
+    <BarcodeScannerModal v-model:barcode="model.barcode" v-model:dialog="showScanner" />
+    <v-chip :value="!!model.barcode" closable @click:close="clearBarcode">{{
+      model.barcode
+    }}</v-chip>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { mdiBarcodeScan } from '@mdi/js'
+
+import BarcodeScannerModal from './modals/BarcodeScannerModal.vue'
+
+const model = defineModel<{ name: string | null; barcode: number | null }>({ required: true })
+const showScanner = ref(false)
+
+const clearBarcode = () => {
+  model.value.barcode = null
+}
+</script>
